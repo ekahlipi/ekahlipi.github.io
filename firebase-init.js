48ccd1119fa22a20d07b5fd3c3f197f0c87b253a
ekahlipi <arobinda2009@gmail.com>
2026-02-06 00:58:33 +0530

    Create firebase-init.js

---
+++ firebase-init.js
@@ -0,0 +1,19 @@
+// firebase-init.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAfINAqiqKQAFqh6k-rvPsF0qF7kQQuIU4",
+  authDomain: "ekahlipi.firebaseapp.com",
+  projectId: "ekahlipi",
+  storageBucket: "ekahlipi.firebasestorage.app",
+  messagingSenderId: "946654489257",
+  appId: "1:946654489257:web:91596b62aecf1a0a5317b8"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const database = getDatabase(app);
+
+export { app, auth, database };
